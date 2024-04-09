818099bf5fd7b59b224724f55f4dffb244bebc45
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2024-04-08 21:18:56 -0300

    first commit

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,27 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => (
+  <aside>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Kevin dos Reis Hartwig</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        KevinReisHartwig
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Full-Stack
+      </Descricao>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
+    </SidebarContainer>
+  </aside>
+)
+
+export default Sidebar
